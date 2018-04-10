5a69417eb24f3c45ef7604c9128abed7c1e3feed
AD\tran.hsp <tran.hsp@tctav.com>
2018-04-10 13:51:21 +0700

    Remove database creation script and complete the remaining of install-magento.sh

--- magento2/install-magento.sh
+++ magento2/install-magento.sh
@@ -3,21 +3,30 @@
 # This is standalone script which configure and install magento
 # -------
 
-MAGENTO_VERSION=2.2.3
+MAGENTO_VERSION_DEFAULT=2.2.3
 PHP_VERSION=7.0
+export AUTHENTICATE_USERNAME=007f25476809ae9622729d03224f7dc6
+export AUTHENTICATE_PASSWORD=b2c2b1fabd3ddde44179c03f453e22da
+export AUTHENTICATE_FILE=~/.composer/auth.json
 export TIME_ZONE="Asia/Ho_Chi_Minh"
 export DEVOPS_HOME=/home/devops
 export BASE_INSTALL=/home/ubuntu/devops/magento2
-export TMP_INSTALL=/tmp/devops-install
 export NGINX_CONF=$BASE_INSTALL/_ubuntu/etc/nginx
+export TMP_INSTALL=/tmp
 export APTVERBOSITY="-qq -y"
 export DEFAULTYESNO="y"
 
+export COMPOSERURL=https://getcomposer.org/installer
 
-export MYSQL_DB_PORT_DEFAULT=3306
-export MYSQL_DB_DRIVER_DEFAULT=com.mysql.jdbc.Driver
-export MYSQL_DB_CONNECTOR_DEFAULT=mysql
-export MYSQL_DB_SUFFIX_DEFAULT="\?useSSL=false\&amp;autoReconnect=true\&amp;useUnicode=yes\&amp;characterEncoding=utf8"
+export MAGENTO_DB_DEFAULT=magento
+export MAGENTO_USER_DEFAULT=magento
+export MAGENTO_DB=$MAGENTO_DB_DEFAULT
+export MAGENTO_USER=$MAGENTO_USER_DEFAULT
+
+#export MYSQL_DB_PORT_DEFAULT=3306
+#export MYSQL_DB_DRIVER_DEFAULT=com.mysql.jdbc.Driver
+#export MYSQL_DB_CONNECTOR_DEFAULT=mysql
+#export MYSQL_DB_SUFFIX_DEFAULT="\?useSSL=false\&amp;autoReconnect=true\&amp;useUnicode=yes\&amp;characterEncoding=utf8"
 
 export WEB_ROOT=/var/www/m2
 
@@ -54,188 +63,201 @@ echogreen "Begin running...."
 echoblue "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
 echo
 
-##
-# Nginx
-##
-echo
-echoblue "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
-echo "Nginx can be used as frontend which redirect request to backend server."
-echo "This installation will add config default proxying to server running behind."
-echoblue "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
-read -e -p "Install nginx${ques} [y/n] " -i "$DEFAULTYESNO" installnginx
-if [ "$installnginx" = "y" ]; then
-
-  # Remove nginx if already installed
-  if [ "`which nginx`" != "" ]; then
-	 sudo apt-get remove --auto-remove nginx nginx-common
-	 sudo apt-get purge --auto-remove nginx nginx-common
-  fi
-  echoblue "Installing nginx. Fetching packages..."
-  echo
-
-#@Deprecated
-#sudo -s << EOF
-#  echo "deb http://nginx.org/packages/mainline/ubuntu $(lsb_release -cs) nginx" >> /etc/apt/sources.list
-#  sudo curl -# -o $TMP_INSTALL/nginx_signing.key http://nginx.org/keys/nginx_signing.key
-#  apt-key add $TMP_INSTALL/nginx_signing.key
-  #echo "deb http://ppa.launchpad.net/nginx/stable/ubuntu $(lsb_release -cs) main" >> /etc/apt/sources.list
-  #apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C300EE8C
-  # Alternate with spdy support and more, change  apt install -> nginx-custom
-  #echo "deb http://ppa.launchpad.net/brianmercer/nginx/ubuntu $(lsb_release -cs) main" >> /etc/apt/sources.list
-  #apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 8D0DC64F
-#EOF
-
-  sudo apt-get $APTVERBOSITY update && sudo apt-get $APTVERBOSITY install nginx
-  # Enable Nginx to auto start when Ubuntu is booted
-  sudo systemctl enable nginx
-
-  # Insert config for letsencrypt
-  if [ ! -d "/opt/letsencrypt/.well-known" ]; then
-	sudo mkdir -p /opt/letsencrypt/.well-known
-	echo "Hello HTTP!" | sudo tee /opt/letsencrypt/index.html
-  fi
-  
-  sudo chown -R www-data:root /opt/letsencrypt
-  
-  if [ -f "/etc/nginx/sites-available/default" ]; then
-	sed -i '/^\(}\)/ i location \/\.well-known {\n  alias \/opt\/letsencrypt\/\.well-known\/;\n  allow all;	\n  }' /etc/nginx/sites-available/default
-  fi
-  
-  
-  ## Reload config file
-  #TODO: sudo service nginx start
-  sudo systemctl restart nginx
-  
-  sudo ufw enable
-  if [ ! -f "/etc/ufw/applications.d/nginx.ufw.profile" ]; then
-	echo "There is no profile for nginx within ufw, so we decide to create it."
-	sudo cat <<EOF >/etc/ufw/applications.d/nginx.ufw.profile
-[Nginx HTTP]
-title=Web Server (Nginx, HTTP)
-description=Small, but very powerful and efficient web server
-ports=80/tcp
-
-[Nginx HTTPS]
-title=Web Server (Nginx, HTTPS)
-description=Small, but very powerful and efficient web server
-ports=443/tcp
-
-[Nginx Full]
-title=Web Server (Nginx, HTTP + HTTPS)
-description=Small, but very powerful and efficient web server
-ports=80,443/tcp
-EOF
-
-	sudo ufw app update nginx
-  fi
-
-  sudo ufw allow 'Nginx HTTP'
-  sudo ufw allow 'Nginx HTTPS'
-  sudo ufw allow 'OpenSSH'
-
-
-  echo
-  echogreen "Finished installing nginx"
-  echo
-else
-  echo "Skipping install of nginx"
+# Create temporary folder for storing downloaded files
+if [ ! -d "$TMP_INSTALL" ]; then
+  mkdir -p $TMP_INSTALL
 fi
 
-
+# Install php
 if [ "`which php`" = "" ]; then
 	
-	# Install php
 	sudo apt-get $APTVERBOSITY install php$PHP_VERSION-fpm php$PHP_VERSION-mcrypt php$PHP_VERSION-curl php$PHP_VERSION-cli php$PHP_VERSION-mysql php$PHP_VERSION-gd php$PHP_VERSION-xsl php$PHP_VERSION-json php$PHP_VERSION-intl php-pear php$PHP_VERSION-dev php$PHP_VERSION-common php$PHP_VERSION-mbstring php$PHP_VERSION-zip php-soap
+fi
+
+# Install composer
+if [ "`which composer`" = "" ]; then
 	
-	# Add php config
-	if [ -f "/etc/php/$PHP_VERSION/fpm/php.ini" ]; then
-		sudo sed -i "s/\(^memory_limit =\).*/\1 1024M/" /etc/php/$PHP_VERSION/fpm/php.ini
-		sudo sed -i "s/\(^max_execution_time =\).*/\1 1800/" /etc/php/$PHP_VERSION/fpm/php.ini
-		sudo sed -i "s/\(^zlib.output_compression =\).*/\1 On/" /etc/php/$PHP_VERSION/fpm/php.ini
-		
-		sudo systemctl restart php7.0-fpm
+  echo "Downloading Composer to temporary folder..."
+  curl -# -o $TMP_INSTALL/composer COMPOSERURL
+  sudo php $TMP_INSTALL/composer
+  
+  # Install composer globally	
+  if [ -f "$BASE_INSTALL/composer.phar" ]; then
+	sudo mv $BASE_INSTALL/composer.phar /usr/local/bin/composer
+  else
+	echo "There is an error while installing composer"
+	exit 1
+  fi
+
+fi
+	
+# Add php config
+if [ -f "/etc/php/$PHP_VERSION/fpm/php.ini" ]; then
+	sudo sed -i "s/\(^memory_limit =\).*/\1 1024M/" /etc/php/$PHP_VERSION/fpm/php.ini
+	sudo sed -i "s/\(^max_execution_time =\).*/\1 1800/" /etc/php/$PHP_VERSION/fpm/php.ini
+	sudo sed -i "s/\(^zlib.output_compression =\).*/\1 On/" /etc/php/$PHP_VERSION/fpm/php.ini
+	
+	sudo systemctl restart php7.0-fpm
+else
+	echo "There is no file php.ini, please check if php is installed correctly."
+fi
+
+if [ ! -d "$WEB_ROOT" ]; then
+	sudo mkdir -p $WEB_ROOT
+fi
+
+# Create authentication file for magento
+if [ ! -f "$AUTHENTICATE_FILE" ]; then
+	echo "Generating magento authentication json."
+	sudo cat <<EOF >$AUTHENTICATE_FILE
+{
+   "http-basic": {
+     "repo.magento.com": {
+        "username":"@@AUTHENTICATE_USERNAME@@",
+        "password":"@@AUTHENTICATE_PASSWORD@@"
+     }
+   }
+} 
+EOF
+
+	sudo sed -i "s/@@AUTHENTICATE_USERNAME@@/$AUTHENTICATE_USERNAME/g" 		$AUTHENTICATE_FILE
+	sudo sed -i "s/@@AUTHENTICATE_PASSWORD@@/$AUTHENTICATE_PASSWORD/g" 		$AUTHENTICATE_FILE
+fi
+
+read -e -p "Please enter the project name${ques} " PROJECT_NAME
+
+if [ -n "$PROJECT_NAME" ]; then
+	cd $WEB_ROOT
+	
+	read -e -p "Please enter the Magento version${ques} " -i "$MAGENTO_VERSION_DEFAULT" MAGENTO_VERSION
+	sudo composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition:$MAGENTO_VERSION $PROJECT_NAME
+	
+	# Install database and setup username password based on project name
+	read -e -p "Create Magento Database and user? [y/n] " -i "y" createdbmagento
+	if [ "$createdbmagento" = "y" ]; then
+	  read -s -p "Enter the Magento database password:"  MAGENTO_PASSWORD
+	  echo ""
+	  read -s -p "Re-Enter the Magento database password:" MAGENTO_PASSWORD2
+	  while [ "$MAGENTO_PASSWORD" != "$MAGENTO_PASSWORD2" ]
+	  do
+		   echo "Password does not match. Please try again"
+	  done
+	  #if [ "$MAGENTO_PASSWORD" == "$MAGENTO_PASSWORD2" ]; then
+		MAGENTO_DB=$PROJECT_NAME
+		MAGENTO_USER=$PROJECT_NAME
+		echo "Creating Magento database and user."
+		echo "You must supply the root user password for MariaDB:"
+		mysql -u root -p << EOF
+	#create workforce db
+	CREATE DATABASE $MAGENTO_DB DEFAULT CHARACTER SET utf8;
+	DELETE FROM mysql.user WHERE User = '$MAGENTO_USER';
+	CREATE USER '$MAGENTO_USER'@'localhost' IDENTIFIED BY '$MAGENTO_PASSWORD';
+	GRANT ALL PRIVILEGES ON $MAGENTO_DB.* TO '$MAGENTO_USER'@'localhost' WITH GRANT OPTION;
+EOF
+	  echo
+	  echo "Remember to update configuration with the Magento database password"
+	  echo
+	  #fi
+	fi
+	
+	# Setup magento
+	read -e -p "Please enter your public host name on this server${ques} " HOSTNAME
+	read -e -p "Please enter the protocol to use for public Share server (http or https)${ques} [https] " -i "https" PROTOCOL
+	if [ -n "$HOSTNAME" ]; then
+		#echo "DB USER : $MAGENTO_USER, DB PASSWORD : $MAGENTO_PASSWORD DB : $MAGENTO_DB"
+		sudo php $WEB_ROOT/$PROJECT_NAME/bin/magento setup:install --base-url=$PROTOCOL://$HOSTNAME --backend-frontname=admin --db-host=127.0.0.1 --db-name=$MAGENTO_DB \
+						--db-password=$MAGENTO_PASSWORD --db-user=$MAGENTO_USER --admin-firstname=admin --admin-lastname=admin --admin-email=admin@mycompany.com \
+						--admin-user=admin --admin-password=$MAGENTO_PASSWORD --language=en_US --currency=USD --timezone=$TIME_ZONE --use-rewrites=1
+						
+		# Set permission on project folder
+		cd $WEB_ROOT/$PROJECT_NAME
+		sudo find var vendor pub/static pub/media app/etc -type f -exec chmod g+w {} \;
+		sudo find var vendor pub/static pub/media app/etc -type d -exec chmod g+ws {} \;
+		sudo chown -R :www-data .		
 	else
-		echo "There is no file php.ini, please check if php is installed correctly."
+		echo "Please input valid hostname"
 	fi
 	
-	if [ ! -d "$WEB_ROOT" ]; then
-		sudo mkdir -p $WEB_ROOT
-	fi
-	
-	cd /var/www/m2
-	
-	read -e -p "Please enter the project name${ques} " PROJECT_NAME
-	
-	if [ -n "$PROJECT_NAME" ]; then
-		sudo composer create-project --repository-url=https://repo.magento.com/magento/project-community-edition:$MAGENTO_VERSION $PROJECT_NAME
-		
-		# Install database and setup username password based on project name
-		. $BASE_INSTALL/scripts/mariadb.sh
-		
-		# Setup magento
-		read -e -p "Please enter your public host name on this server${ques} " HOSTNAME
-		if [ -n "$HOSTNAME" ]; then
-			sudo php bin/magento setup:install --base-url=http://$HOSTNAME --backend-frontname=admin --db-host=127.0.0.1 --db-name=$MAGENTO_DB \
-							--db-password=$MAGENTO_PASSWORD --db-user=$MAGENTO_USER --admin-firstname=admin --admin-lastname=admin --admin-email=admin@mycompany.com \
-							--admin-user=admin --admin-password=$MAGENTO_PASSWORD --language=en_US --currency=USD --timezone=$TIME_ZONE --use-rewrites=1
-		else
-			echo "Please input valid hostname"
+	# Setup SSL
+	#read -e -p "Install SSL${ques} [y/n] " -i "$DEFAULTYESNO" installssl
+	if [ "$PROTOCOL" = "https" ]; then
+		local_port=443
+		echo "SSL for domain : $HOSTNAME is being created with port : $local_port"
+		if [ ! -f "/etc/letsencrypt/live/$HOSTNAME/fullchain.pem" ]; then
+			sudo certbot certonly --authenticator standalone --installer nginx -d $HOSTNAME --pre-hook "systemctl stop nginx" --post-hook "systemctl start nginx"
 		fi
 		
-		# Setup SSL
-		read -e -p "Install SSL${ques} [y/n] " -i "$DEFAULTYESNO" installssl
-		if [ "$installssl" = "y" ]; then
-			local_port=443
-			echo "SSL for domain : $HOSTNAME is being created with port : $local_port"
-			if [ ! -f "/etc/letsencrypt/live/$HOSTNAME/fullchain.pem" ]; then
-				sudo certbot certonly --authenticator standalone --installer nginx -d $HOSTNAME --pre-hook "systemctl stop nginx" --post-hook "systemctl start nginx"
-			fi
-			
-			if [ -f "/etc/letsencrypt/live/$HOSTNAME/fullchain.pem" ]; then
-			
-				sudo rsync -avz $NGINX_CONF/snippets/ /etc/nginx/snippets/
-				sudo rsync -avz $NGINX_CONF/sites-available/domain.conf.ssl /etc/nginx/sites-available/$HOSTNAME.conf
-				sudo ln -s /etc/nginx/sites-available/$HOSTNAME.conf /etc/nginx/sites-enabled/
-				  
-				sudo sed -i "s/@@WEB_ROOT@@/${WEB_ROOT//\//\\/}/g" /etc/nginx/sites-available/$HOSTNAME.conf
-				sudo sed -i "s/@@DNS_DOMAIN@@/$hostname/g" /etc/nginx/sites-available/$HOSTNAME.conf
-
-			
-				sudo sed -i "s/@@PORT@@/8080/g" /etc/nginx/sites-available/$HOSTNAME.conf
-				
-					
-				# Add cron job to renew key
-				crontab -l | { cat; echo '43 6 * * * root /usr/bin/certbot renew --post-hook "systemctl reload nginx" > /var/log/certbot-renew.log'; } | crontab -
-				  
-				echo "SSL for domain : $HOSTNAME has been created successfully."
-				  
-			else
-				  echored "There is an error in generating keys for domain $HOSTNAME."
-			fi
-
-			
-		else
-			
+		if [ -f "/etc/letsencrypt/live/$HOSTNAME/fullchain.pem" ]; then
+		
 			sudo cat <<EOF >/etc/nginx/sites-available/$HOSTNAME.conf
+server {
+  listen 80;
+  server_name @@DNS_DOMAIN@@;
+ 
+  include /etc/nginx/snippets/ssl.conf;
+
+  location / {
+    return 301 https://\$host\$request_uri;
+  }
+}
+
+upstream fastcgi_backend {
+  server  unix:/run/php/php7.0-fpm.sock;
+}
+
+server {
+  server_name @@DNS_DOMAIN@@;
+  listen 443 ssl http2;
+  
+  ssl_certificate /etc/letsencrypt/live/@@DNS_DOMAIN@@/fullchain.pem;
+  ssl_certificate_key /etc/letsencrypt/live/@@DNS_DOMAIN@@/privkey.pem;
+  ssl_trusted_certificate /etc/letsencrypt/live/@@DNS_DOMAIN@@/fullchain.pem;
+  include /etc/nginx/snippets/ssl.conf;
+  
+  access_log /var/log/nginx/magento2-access.log;
+  error_log /var/log/nginx/magento2-error.log;
+
+  set \$MAGE_ROOT @@ROOT_PROJECT_FOLDER@@;
+  set \$MAGE_MODE developer;
+  include @@ROOT_PROJECT_FOLDER@@/nginx.conf.sample;
+}
+EOF
+			# Replace template with configuration value created in previous step
+			sudo sed -i "s/@@DNS_DOMAIN@@/$HOSTNAME/g" 		/etc/nginx/sites-available/$HOSTNAME.conf
+			sudo sed -i "s/@@ROOT_PROJECT_FOLDER@@/$WEB_ROOT_PATH\/$PROJECT_NAME/g" 	/etc/nginx/sites-available/$HOSTNAME.conf
+				
+			# Add cron job to renew key
+			crontab -l | { cat; echo '43 6 * * * root /usr/bin/certbot renew --post-hook "systemctl reload nginx" > /var/log/certbot-renew.log'; } | crontab -
+			  
+			echo "SSL for domain : $HOSTNAME has been created successfully."
+			  
+		else
+			  echored "There is an error in generating keys for domain $HOSTNAME."
+		fi
+
+		
+	else
+		
+		sudo cat <<EOF >/etc/nginx/sites-available/$HOSTNAME.conf
 upstream fastcgi_backend {
      server  unix:/run/php/php7.0-fpm.sock;
  }
 
  server {
      listen 80;
-     server_name @@DOMAIN_NAME@@;
-     set $MAGE_ROOT @@ROOT_PROJECT_FOLDER@@;
+     server_name @@DNS_DOMAIN@@;
+     set \$MAGE_ROOT @@ROOT_PROJECT_FOLDER@@;
      include @@ROOT_PROJECT_FOLDER@@/nginx.conf.sample;
  }
 EOF
-			# Replace template with configuration value created in previous step
-			sudo sed -i "s/@@DOMAIN_NAME@@/$HOSTNAME/g" 		/etc/nginx/sites-available/$HOSTNAME.conf
-			sudo sed -i "s/@@ROOT_PROJECT_FOLDER@@/$$WEB_ROOT_PATH\/$PROJECT_NAME/g" 	/etc/nginx/sites-available/$HOSTNAME.conf
-			
-		fi
-	
-	else
-		echo "Please input valid name for creating project"
+		# Replace template with configuration value created in previous step
+		sudo sed -i "s/@@DNS_DOMAIN@@/$HOSTNAME/g" 		/etc/nginx/sites-available/$HOSTNAME.conf
+		sudo sed -i "s/@@ROOT_PROJECT_FOLDER@@/$WEB_ROOT_PATH\/$PROJECT_NAME/g" 	/etc/nginx/sites-available/$HOSTNAME.conf
+		sudo service nginx restart
 	fi
-	
+
+else
+	echo "Please input valid name for creating project"
 fi
+	
+
