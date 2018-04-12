5a6d639cd697f4a5957278c2a43bf60adac56493
AD\tran.hsp <tran.hsp@tctav.com>
2018-04-12 11:07:05 +0700

    Remove some magento constants and change database installation to allow for selection

--- magento2/install-lemp.sh
+++ magento2/install-lemp.sh
@@ -218,20 +218,6 @@ if [ "`which mysql`" = "" ]; then
 			;;
         *) echo "invalid option";;
     esac
-	read -e -p "Install MariaDB? [y/n] " -i "y" installmariadb
-	if [ "$installmariadb" = "y" ]; then
-	  sudo apt-get install software-properties-common
-	  sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
-	  sudo add-apt-repository "deb [arch=amd64,i386,ppc64el] http://ftp.ddg.lth.se/mariadb/repo/10.1/ubuntu $(lsb_release -cs) main"
-	  sudo apt-get update
-	  sudo apt-get $APTVERBOSITY install mariadb-server
-	  sudo mysql_secure_installation
-	  #Tuning database by setting config
-	  echo "key_buffer_size         = 128M" >> /etc/mysql/conf.d/mariadb.cnf
-	  echo "max_allowed_packet      = 128M" >> /etc/mysql/conf.d/mariadb.cnf
-	  echo "thread_stack            = 1024K" >> /etc/mysql/conf.d/mariadb.cnf
-	  echo "innodb_log_file_size    = 128M" >> /etc/mysql/conf.d/mariadb.cnf
-	fi
 fi
 
 	
