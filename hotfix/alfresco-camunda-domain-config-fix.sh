2536d6b1e27dd7b8836735fc9bb2fb62080f14bf
AD\tran.hsp <tran.hsp@tctav.com>
2018-04-16 13:47:02 +0700

    Fix alfresco and camunda domain

---
+++ hotfix/alfresco-camunda-domain-config-fix.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+# -------
+# This is standalone script which fix domain configuration for alfresco and camunda
+# -------
+
+# Configure constants
+if [ -f "../constants.sh" ]; then
+	. ../constants.sh
+fi
+
+# Configure colors
+if [ -f "../colors.sh" ]; then
+	. ../colors.sh
+fi
+
+echo
+echoblue "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+echogreen "Begin running...."
+echoblue "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+echo
+
+
+read -e -p "Please enter the public host name for Alfresco server (fully qualified domain name)${ques} [`hostname`] " -i "`hostname`" SHARE_HOSTNAME
+	
+if [ -f "$NGINX_CONF/sites-available/$SHARE_HOSTNAME.conf" ]; then
+	# Remove old configuration
+	rm $NGINX_CONF/sites-available/$SHARE_HOSTNAME.conf
+fi
+
+. $BASE_INSTALL/ssl.sh	$SHARE_HOSTNAME
+
+# Insert cache config
+sudo sed -i '1 i\proxy_cache_path \/var\/cache\/nginx\/alfresco levels=1 keys_zone=alfrescoscache:256m max_size=512m inactive=1440m;\n' /etc/nginx/sites-available/$SHARE_HOSTNAME.conf
+
+sudo sed -i "0,/server/s/server/upstream alfresco {	\n\tserver localhost\:$TOMCAT_HTTP_PORT;	\n}	\n\n upstream share {    \n\tserver localhost:$TOMCAT_HTTP_PORT;	\n}\n\n&/" /etc/nginx/sites-available/$SHARE_HOSTNAME.conf
+
+sudo sed -i "s/##REWRITE##/rewrite \^\/\$	\/share;/g" /etc/nginx/sites-available/$SHARE_HOSTNAME.conf
+
+# Insert alfresco configuration content before the last line in domain.conf in nginx
+sudo mkdir temp
+sudo cp $NGINX_CONF/sites-available/alfresco.snippet	temp/
+sudo sed -e '/##ALFRESCO##/ {' -e 'r temp/alfresco.snippet' -e 'd' -e '}' -i /etc/nginx/sites-available/$SHARE_HOSTNAME.conf
+sudo rm -rf temp
+
+sudo mkdir -p /var/cache/nginx/alfresco
+  
+sudo chown -R www-data:root /var/cache/nginx/alfresco
+
+read -e -p "Please enter the public host name for Camunda server (fully qualified domain name)${ques} [`hostname`] " -i "`hostname`" CAMUNDA_HOSTNAME
+
+if [ -f "$NGINX_CONF/sites-available/$CAMUNDA_HOSTNAME.conf" ]; then
+	# Remove old configuration
+	rm $NGINX_CONF/sites-available/$CAMUNDA_HOSTNAME.conf
+fi
+
+
+# Create a new one to remove common snippet
+.	$BASE_INSTALL/ssl.sh $CAMUNDA_HOSTNAME
+
+  
+#echo "Installing configuration for camunda on nginx..."
+	
+if [ -f "/etc/nginx/sites-available/$CAMUNDA_HOSTNAME.conf" ]; then
+ sudo sed -i "0,/server/s/server/upstream camunda {    \n\tserver localhost\:$TOMCAT_HTTP_PORT;	\n}	\n\n	upstream engine-rest {	    \n\tserver localhost:$TOMCAT_HTTP_PORT;	\n}\n\n&/" /etc/nginx/sites-available/$CAMUNDA_HOSTNAME.conf
+ 
+ sudo sed -i "s/##REWRITE##/rewrite \^\/\$	\/share;/g" /etc/nginx/sites-available/$CAMUNDA_HOSTNAME.conf
+ 
+ # Insert camunda configuration content before the last line in domain.conf in nginx
+ #sudo sed -i "$e cat $NGINX_CONF/sites-available/camunda.conf" /etc/nginx/sites-available/$hostname.conf
+ sudo mkdir temp
+ sudo cp $NGINX_CONF/sites-available/camunda.snippet	temp/
+ sudo sed -e '/##CAMUNDA##/ {' -e 'r temp/camunda.snippet' -e 'd' -e '}' -i /etc/nginx/sites-available/$CAMUNDA_HOSTNAME.conf
+ sudo rm -rf temp
+fi
