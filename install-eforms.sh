e457e1c3310f6713d973efa0664d4c7951f55599
AD\tran.hsp <tran.hsp@tctav.com>
2018-04-16 10:06:09 +0700

    Add script to setup eforms project

---
+++ install-eforms.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# -------
+# This is common script which will be called to generate SSL
+# -------
+
+# Configure constants
+if [ -f "constants.sh" ]; then
+	. constants.sh
+fi
+
+# Configure colors
+if [ -f "colors.sh" ]; then
+	. colors.sh
+fi
+
+echogreen "Setting up Eforms Camunda..........."
+
+if [ "`which git`" = "" ]; then
+	echoblue "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+	echored "Please run ubuntu-upgrade.sh firstly to install git before running this script."
+	echoblue "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+fi
+
+if [ -d "$TMP_INSTALL/workplacebpm" ]; then
+	sudo rm -rf $TMP_INSTALL/workplacebpm
+fi
+
+sudo git clone https://bitbucket.org/workplace101/workplacebpm 	$TMP_INSTALL/workplacebpm
+cd $TMP_INSTALL/workplacebpm/src/eForm
+source /etc/profile.d/maven.sh
+mvn clean install
+sudo rsync -avz $TMP_INSTALL/workplacebpm/src/eForm/gateway/target/eform.war $CATALINA_HOME/webapps
