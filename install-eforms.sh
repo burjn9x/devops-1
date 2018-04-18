470895373a0e9d6ec8f5ca6faae238cd0402eb2c
AD\tran.hsp <tran.hsp@tctav.com>
2018-04-18 18:08:03 +0700

    update eform install

--- install-eforms.sh
+++ install-eforms.sh
@@ -32,6 +32,7 @@ mvn clean install
 sudo rsync -avz $TMP_INSTALL/workplacebpm/src/eForm/gateway/target/eform.war $CATALINA_HOME/webapps
 
 read -e -p "Please enter the public host name for Camunda server (fully qualified domain name)${ques} [`hostname`] " -i "`hostname`" CAMUNDA_HOSTNAME
+read -e -p "Please enter the public host name for Alfresco server (fully qualified domain name)${ques} [`hostname`] " -i "`hostname`" ALFRESCO_HOSTNAME
 	
 if [ -f "/etc/nginx/sites-available/$CAMUNDA_HOSTNAME.conf" ]; then
 	sudo sed -i "0,/server/s/server/upstream eform {	    \n\tserver localhost:$TOMCAT_HTTP_PORT;	\n}\n\n&/" /etc/nginx/sites-available/$CAMUNDA_HOSTNAME.conf
@@ -48,9 +49,9 @@ echo "We are waiting for eform being deployed...."
 
 sleep 20
 
-sudo sed -i "s/\(^endpoint=\).*/\1https\:\/\/scaucwnkwa.execute-api.ap-southeast-1.amazonaws.com\/v1\/notify\/workchat/"  $CATALINA_HOME/webapps/eform/WEB-INF/classes/alfresco-global.properties
-sudo sed -i "s/\(^CmisBrowserUrl=\).*/\1http:\/\/$CAMUNDA_HOSTNAME\/alfresco\/api\/-default-\/public\/cmis\/versions\/1.1\/browser/"  $CATALINA_HOME/webapps/eform/WEB-INF/classes/alfresco-global.properties
-sudo sed -i "s/\(^CmisRepoId=\).*/\1-default-/"  $CATALINA_HOME/shared/classes/alfresco-global.properties 	$CATALINA_HOME/webapps/eform/WEB-INF/classes/alfresco-global.properties
-sudo sed -i "s/\(^CmisUser=\).*/\1admin/"  $CATALINA_HOME/shared/classes/alfresco-global.properties		$CATALINA_HOME/webapps/eform/WEB-INF/classes/alfresco-global.properties
-sudo sed -i "s/\(^CmisPassword=\).*/\1admin/"  $CATALINA_HOME/shared/classes/alfresco-global.properties		$CATALINA_HOME/webapps/eform/WEB-INF/classes/alfresco-global.properties
-sudo sed -i "s/\(^CmisRootFolder=\).*/\Data Dictionary/"  $CATALINA_HOME/shared/classes/alfresco-global.properties	$CATALINA_HOME/webapps/eform/WEB-INF/classes/alfresco-global.properties
+sudo sed -i "s/\(^endpoint=\).*/\1https\:\/\/scaucwnkwa.execute-api.ap-southeast-1.amazonaws.com\/v1\/notify\/workchat/"  $CATALINA_HOME/webapps/eform/WEB-INF/classes/application.properties
+sudo sed -i "s/\(^CmisBrowserUrl=\).*/\1https:\/\/$ALFRESCO_HOSTNAME\/alfresco\/api\/-default-\/public\/cmis\/versions\/1.1\/browser/"  $CATALINA_HOME/webapps/eform/WEB-INF/classes/application.properties
+sudo sed -i "s/\(^CmisRepoId=\).*/\1-default-/" 	$CATALINA_HOME/webapps/eform/WEB-INF/classes/application.properties
+sudo sed -i "s/\(^CmisUser=\).*/\1admin/"  	$CATALINA_HOME/webapps/eform/WEB-INF/classes/application.properties
+sudo sed -i "s/\(^CmisPassword=\).*/\1admin/"  	$CATALINA_HOME/webapps/eform/WEB-INF/classes/application.properties
+sudo sed -i "s/\(^CmisRootFolder=\).*/\Data Dictionary/"  $CATALINA_HOME/webapps/eform/WEB-INF/classes/application.properties
