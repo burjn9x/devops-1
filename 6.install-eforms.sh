923c210f663131752b756461b45d6a866753b862
AD\tran.hsp <tran.hsp@tctav.com>
2018-05-07 15:54:22 +0700

    Remove sudo from git clone

--- 6.install-eforms.sh
+++ 6.install-eforms.sh
@@ -25,7 +25,7 @@ if [ -d "$TMP_INSTALL/workplacebpm" ]; then
 	sudo rm -rf $TMP_INSTALL/workplacebpm
 fi
 
-sudo git clone https://bitbucket.org/workplace101/workplacebpm.git $TMP_INSTALL/workplacebpm
+git clone https://bitbucket.org/workplace101/workplacebpm.git $TMP_INSTALL/workplacebpm
 cd $TMP_INSTALL/workplacebpm/src/eForm
 source /etc/profile.d/maven.sh
 mvn clean install
@@ -79,15 +79,15 @@ sudo sed -i "s/\(^CmisRootFolder=\).*/\Data Dictionary/"  $CATALINA_HOME/webapps
 . $DEVOPS_HOME/devops-service.sh restart
 
 # Eform camunda UI
-sudo git clone https://DigitalBusiness@bitbucket.org/workplace101/eformcamundaui.git $TMP_INSTALL/eformcamundaui
+git clone https://DigitalBusiness@bitbucket.org/workplace101/eformcamundaui.git $TMP_INSTALL/eformcamundaui
 npm install -g grunt-cli
 cd $TMP_INSTALL/eformcamundaui
 grunt
 sudo rsync -avz $TMP_INSTALL/eformcamundaui/taget/webapp/* 	$CATALINA_HOME/webapps/camunda/
 
 # EForm Renderer
-sudo git clone https://bitbucket.org/workplace101/eformsrenderer.git $DEVOPS_HOME/eformsrenderer
-sudo git clone https://bitbucket.org/workplace101/eforms-builder.git $DEVOPS_HOME/eforms-builder
+git clone https://bitbucket.org/workplace101/eformsrenderer.git $DEVOPS_HOME/eformsrenderer
+git clone https://bitbucket.org/workplace101/eforms-builder.git $DEVOPS_HOME/eforms-builder
 npm install -g @angular/cli
 npm install
 npm run build
