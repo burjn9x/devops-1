ab9b763f51b7641f1d3df8b3af0687cd40349438
AD\tran.hsp <tran.hsp@tctav.com>
2018-05-15 17:14:09 +0700

    deploy with root permission

--- 91.deploy-dmn.sh
+++ 91.deploy-dmn.sh
@@ -77,4 +77,4 @@ cd $TMP_INSTALL/workplacebpm/src/workforce-dmn-xlsx
 source /etc/profile.d/maven.sh
 mvn clean install -Dmaven.test.skip=true
 
-java -jar $TMP_INSTALL/workplacebpm/src/workforce-dmn-xlsx/xlsx-dmn-cli/target/dmn-xlsx-cli-0.1.2-SNAPSHOT.jar
+sudo java -jar $TMP_INSTALL/workplacebpm/src/workforce-dmn-xlsx/xlsx-dmn-cli/target/dmn-xlsx-cli-0.1.2-SNAPSHOT.jar
