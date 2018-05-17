a955c382e01d5654fd9761bb3f7dd16742af41a2
AD\tran.hsp <tran.hsp@tctav.com>
2018-05-17 14:36:29 +0700

    Add BPMN into deployment

--- 91.deploy-dmn.sh
+++ 91.deploy-dmn.sh
@@ -12,6 +12,9 @@ if [ -f "colors.sh" ]; then
 	. colors.sh
 fi
 
+export BPMN_PATH=$DEVOPS_HOME/eforms/bpmn/BusinessTrip.bpmn
+
+
 echogreen "Begin running script to convert file..........."
 
 if [ -d "$TMP_INSTALL/workplacebpm" ]; then
@@ -20,10 +23,15 @@ fi
 
 git clone https://bitbucket.org/workplace101/workplacebpm.git $TMP_INSTALL/workplacebpm
 
+if [ ! -d "$DEVOPS_HOME/eforms/bpmn" ]; then
+	mkdir -p $DEVOPS_HOME/eforms/bpmn
+	echored "There is no bpmn input file for this dmn conversion, please put your BPMN which will be deployed along with generated DMN..."
+	exit 1
+fi
 
-if [ ! -d "$DEVOPS_HOME/dmn/input" ]; then
-	mkdir -p $DEVOPS_HOME/dmn/input
-	echored "There is no input file for this dmn conversion..."
+if [ ! -d "$DEVOPS_HOME/eforms/dmn/input" ]; then
+	mkdir -p $DEVOPS_HOME/eforms/dmn/input
+	echored "There is no dmn input file for this dmn conversion..."
 	exit 1
 fi
 
@@ -33,10 +41,10 @@ if [ ! -f "$DEVOPS_HOME/dmn/input/RACI-Decision-Making-Criteria.xlsx" ]; then
 	exit 1
 fi
 
-if [ ! -d "$DEVOPS_HOME/workplacebpm/src/workforce-dmn-xlsx/output" ]; then
-	mkdir -p $DEVOPS_HOME/dmn/output
+if [ ! -d "$DEVOPS_HOME/eforms/dmn/output" ]; then
+	mkdir -p $DEVOPS_HOME/eforms/dmn/output
 else
-	sudo rm -rf $DEVOPS_HOME/dmn/output/*.*
+	sudo rm -rf $DEVOPS_HOME/eforms/dmn/output/*.*
 fi
 
 read -e -p "Please enter the tenant id for DMN deployment${ques} [TTV] " -i "TTV" TENANT_ID
@@ -45,17 +53,18 @@ read -e -p "Please enter the public host name for your server (only domain name,
 sudo sed -i "s/MYCOMPANY.COM/$DOMAIN_NAME/g" $BASE_INSTALL/domain.txt
 
 DEVOPS_HOME_PATH="${DEVOPS_HOME//\//\\/}"
-sudo sed -i "s/\(^eform.cli.raci.filePath=\).*/\1$DEVOPS_HOME_PATH\/dmn\/input\/RACI-Decision-Making-Criteria.xlsx/" 	$TMP_INSTALL/workplacebpm/src/workforce-dmn-xlsx/xlsx-dmn-cli/src/main/resources/application.properties
-sudo sed -i "s/\(^eform.cli.departmentMaster.filePath=\).*/\1$DEVOPS_HOME_PATH\/dmn\/input\/RACI-Decision-Making-Criteria.xlsx/" 	$TMP_INSTALL/workplacebpm/src/workforce-dmn-xlsx/xlsx-dmn-cli/src/main/resources/application.properties
-sudo sed -i "s/\(^eform.cli.bom.filePath=\).*/\1$DEVOPS_HOME_PATH\/dmn\/input\/RACI-Decision-Making-Criteria.xlsx/" 	$TMP_INSTALL/workplacebpm/src/workforce-dmn-xlsx/xlsx-dmn-cli/src/main/resources/application.properties
-sudo sed -i "s/\(^eform.cli.bufferDepartment.filePath=\).*/\1$DEVOPS_HOME_PATH\/dmn\/input\/RACI-Decision-Making-Criteria.xlsx/" 	$TMP_INSTALL/workplacebpm/src/workforce-dmn-xlsx/xlsx-dmn-cli/src/main/resources/application.properties
-sudo sed -i "s/\(^eform.cli.dmn.bom.outputFilePath=\).*/\1$DEVOPS_HOME_PATH\/dmn\/output\/BOMApproval.dmn/" 	$TMP_INSTALL/workplacebpm/src/workforce-dmn-xlsx/xlsx-dmn-cli/src/main/resources/application.properties
-sudo sed -i "s/\(^eform.cli.dmn.bom.bufferFilePath=\).*/\1$DEVOPS_HOME_PATH\/dmn\/input\/RACI-Decision-Making-Criteria.xlsx/" 	$TMP_INSTALL/workplacebpm/src/workforce-dmn-xlsx/xlsx-dmn-cli/src/main/resources/application.properties
+sudo sed -i "s/\(^eform.cli.raci.filePath=\).*/\1$DEVOPS_HOME_PATH\/eforms\/dmn\/input\/RACI-Decision-Making-Criteria.xlsx/" 	$TMP_INSTALL/workplacebpm/src/workforce-dmn-xlsx/xlsx-dmn-cli/src/main/resources/application.properties
+sudo sed -i "s/\(^eform.cli.departmentMaster.filePath=\).*/\1$DEVOPS_HOME_PATH\/eforms\/dmn\/input\/RACI-Decision-Making-Criteria.xlsx/" 	$TMP_INSTALL/workplacebpm/src/workforce-dmn-xlsx/xlsx-dmn-cli/src/main/resources/application.properties
+sudo sed -i "s/\(^eform.cli.bom.filePath=\).*/\1$DEVOPS_HOME_PATH\/eforms\/dmn\/input\/RACI-Decision-Making-Criteria.xlsx/" 	$TMP_INSTALL/workplacebpm/src/workforce-dmn-xlsx/xlsx-dmn-cli/src/main/resources/application.properties
+sudo sed -i "s/\(^eform.cli.bufferDepartment.filePath=\).*/\1$DEVOPS_HOME_PATH\/eforms\/dmn\/input\/RACI-Decision-Making-Criteria.xlsx/" 	$TMP_INSTALL/workplacebpm/src/workforce-dmn-xlsx/xlsx-dmn-cli/src/main/resources/application.properties
+sudo sed -i "s/\(^eform.cli.dmn.bom.outputFilePath=\).*/\1$DEVOPS_HOME_PATH\/eforms\/dmn\/output\/BOMApproval.dmn/" 	$TMP_INSTALL/workplacebpm/src/workforce-dmn-xlsx/xlsx-dmn-cli/src/main/resources/application.properties
+sudo sed -i "s/\(^eform.cli.dmn.bom.bufferFilePath=\).*/\1$DEVOPS_HOME_PATH\/eforms\/dmn\/input\/RACI-Decision-Making-Criteria.xlsx/" 	$TMP_INSTALL/workplacebpm/src/workforce-dmn-xlsx/xlsx-dmn-cli/src/main/resources/application.properties
 
-sudo sed -i "s/\(^eform.cli.dmn.department.outputFilePath=\).*/\1$DEVOPS_HOME_PATH\/dmn\/output\/DepartmentApproval.dmn/" 	$TMP_INSTALL/workplacebpm/src/workforce-dmn-xlsx/xlsx-dmn-cli/src/main/resources/application.properties
-sudo sed -i "s/\(^eform.cli.dmn.department.bufferFilePath=\).*/\1$DEVOPS_HOME_PATH\/dmn\/input\/RACI-Decision-Making-Criteria.xlsx/" 	$TMP_INSTALL/workplacebpm/src/workforce-dmn-xlsx/xlsx-dmn-cli/src/main/resources/application.properties
+sudo sed -i "s/\(^eform.cli.dmn.department.outputFilePath=\).*/\1$DEVOPS_HOME_PATH\/eforms\/dmn\/output\/DepartmentApproval.dmn/" 	$TMP_INSTALL/workplacebpm/src/workforce-dmn-xlsx/xlsx-dmn-cli/src/main/resources/application.properties
+sudo sed -i "s/\(^eform.cli.dmn.department.bufferFilePath=\).*/\1$DEVOPS_HOME_PATH\/eforms\/dmn\/input\/RACI-Decision-Making-Criteria.xlsx/" 	$TMP_INSTALL/workplacebpm/src/workforce-dmn-xlsx/xlsx-dmn-cli/src/main/resources/application.properties
 
-sudo sed -i "s/\(^eform.cli.bpmn.deployFilePath=\).*/\1/" 	$TMP_INSTALL/workplacebpm/src/workforce-dmn-xlsx/xlsx-dmn-cli/src/main/resources/application.properties
+BPMN_PATH_ESC="${BPMN_PATH//\//\\/}"
+sudo sed -i "s/\(^eform.cli.bpmn.deployFilePath=\).*/\1$BPMN_PATH_ESC/" 	$TMP_INSTALL/workplacebpm/src/workforce-dmn-xlsx/xlsx-dmn-cli/src/main/resources/application.properties
 
 camunda_line=$(grep "camunda" $BASE_INSTALL/domain.txt)
 IFS='|' read -ra arr <<<"$camunda_line"
@@ -80,24 +89,24 @@ mvn clean install -Dmaven.test.skip=true
 sudo java -jar $TMP_INSTALL/workplacebpm/src/workforce-dmn-xlsx/xlsx-dmn-cli/target/dmn-xlsx-cli-0.1.2-SNAPSHOT.jar
 
 # Copy generated DMN into eform source and deploy eform
-sudo rsync -avz $DEVOPS_HOME_PATH/dmn/output/*.dmn $TMP_INSTALL/workplacebpm/src/eForm/workflow/src/main/resources/processes/$TENANT_ID
-cd $TMP_INSTALL/workplacebpm/src/eForm
+# sudo rsync -avz $DEVOPS_HOME_PATH/dmn/output/*.dmn $TMP_INSTALL/workplacebpm/src/eForm/workflow/src/main/resources/processes/$TENANT_ID
+# cd $TMP_INSTALL/workplacebpm/src/eForm
 
-mkdir $TMP_INSTALL/temp
-cp $CATALINA_HOME/webapps/eform/WEB-INF/classes/application.properties $TMP_INSTALL/temp
-mvn clean install
-if [ -d "$CATALINA_HOME/webapps/eform" ]; then
-	sudo rm -rf $CATALINA_HOME/webapps/eform*
-fi
-sudo rsync -avz $TMP_INSTALL/workplacebpm/src/eForm/gateway/target/eform.war $CATALINA_HOME/webapps
+# mkdir $TMP_INSTALL/temp
+# cp $CATALINA_HOME/webapps/eform/WEB-INF/classes/application.properties $TMP_INSTALL/temp
+# mvn clean install
+# if [ -d "$CATALINA_HOME/webapps/eform" ]; then
+	# sudo rm -rf $CATALINA_HOME/webapps/eform*
+# fi
+# sudo rsync -avz $TMP_INSTALL/workplacebpm/src/eForm/gateway/target/eform.war $CATALINA_HOME/webapps
 
-echo "We are waiting for eform being deployed...."
+# echo "We are waiting for eform being deployed...."
 
-sleep 20
+# sleep 20
 
-sudo rsync -avz $TMP_INSTALL/temp/application.properties $CATALINA_HOME/webapps/eform/WEB-INF/classes/application.properties
+# sudo rsync -avz $TMP_INSTALL/temp/application.properties $CATALINA_HOME/webapps/eform/WEB-INF/classes/application.properties
 
-sudo rm -rf $TMP_INSTALL/temp
+# sudo rm -rf $TMP_INSTALL/temp
 
-sudo $DEVOPS_HOME/devops-service.sh restart
+# sudo $DEVOPS_HOME/devops-service.sh restart
 
