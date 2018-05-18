c1571bc0fa2635e1706ece4d3b78f96de814fcdb
AD\tran.hsp <tran.hsp@tctav.com>
2018-05-18 12:58:35 +0700

    keep original name

--- 6.install-camunda-insurance.sh
+++ 6.install-camunda-insurance.sh
@@ -24,15 +24,15 @@ git clone https://bitbucket.org/workplace101/workplacebpm.git $TMP_INSTALL/workp
 cd $TMP_INSTALL/workplacebpm/src/camunda-showcase-insurance-application-master
 mvn clean install -Dmaven.test.skip=true
 
-if [ -d "$CATALINA_HOME/webapps/camunda-insurance" ]; then
-	sudo rm -rf $CATALINA_HOME/webapps/camunda-insurance*
+if [ -d "$CATALINA_HOME/webapps/camunda-showcase-insurance-application" ]; then
+	sudo rm -rf $CATALINA_HOME/webapps/camunda-showcase-insurance-application*
 fi
-sudo rsync -avz $TMP_INSTALL/workplacebpm/src/camunda-showcase-insurance-application-master/target/camunda-showcase-insurance-application.war $CATALINA_HOME/webapps/insurance.war
+sudo rsync -avz $TMP_INSTALL/workplacebpm/src/camunda-showcase-insurance-application-master/target/camunda-showcase-insurance-application.war $CATALINA_HOME/webapps/camunda-showcase-insurance-application.war
 
 sleep 20
 
 # Use version 2.0 rather than 1.1
-sudo rm  $CATALINA_HOME/webapps/insurance/WEB-INF/lib/jsr311-api-1.1.1.jar
+sudo rm  $CATALINA_HOME/webapps/camunda-showcase-insurance-application/WEB-INF/lib/jsr311-api-1.1.1.jar
 
 # Use newer version of mysql connector
 sudo rm  $CATALINA_HOME/lib/mysql-connector-java*.jar
