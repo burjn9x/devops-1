d8456b255db753693a87d810ed3226ebea17c045
Thanh Tran <thanh.tn@tctav.com>
2018-05-23 09:12:43 +0700

    add script create tenant, user

---
+++ 10.create-tenant.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# -------
+# This is script to create tenant
+
+# Configure colors
+if [ -f "colors.sh" ]; then
+	. colors.sh
+fi
+
+echogreen "Begin running script to create tenant..........."
+
+echo -ne "Name of tenant you want to create :"
+read tenantId
+
+curl -G localhost:8181//eform/tenant/$tenantId
