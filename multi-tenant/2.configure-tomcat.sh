cc249f98def62f447d611da8415c60db5c1d0eb3
Thanh Tran <thanh.tn@tctav.com>
2018-05-24 12:20:42 +0700

    update script install multitenant

--- multi-tenant/2.configure-tomcat.sh
+++ multi-tenant/2.configure-tomcat.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # -------
-# This is script to setup eform workplace
+# This is script to setup tomcat
 # -------
 
 # Configure constants
@@ -13,28 +13,64 @@ if [ -f "colors.sh" ]; then
 	. colors.sh
 fi
 
-sudo sed -i '/<\/GlobalNamingResources>/i \
-		<Resource name="jdbc\/ProcessEngine"\
-				  auth="Container"\
-				  type="javax.sql.DataSource"\
-				  factory="org.apache.tomcat.jdbc.pool.DataSourceFactory"\
-				  uniqueResourceName="process-engine"\
-				  driverClassName="@@DB_DRIVER@@"\
-				  url="jdbc:@@DB_CONNECTOR@@:\/\/localhost:@@DB_PORT@@\/@@TTV_DB@@@@DB_SUFFIX@@"\
-				  username="@@TTV_USER@@"\
-				  password="@@TTV_PASSWORD@@"\
-				  maxActive="20"\
-				  minIdle="5"\/> ' $CATALINA_HOME/conf/server.xml
 
-sudo sed -i '/<\/GlobalNamingResources>/i \
-		<Resource name="jdbc\/ProcessEngine"\
-				  auth="Container"\
-				  type="javax.sql.DataSource"\
-				  factory="org.apache.tomcat.jdbc.pool.DataSourceFactory"\
-				  uniqueResourceName="process-engine"\
-				  driverClassName="@@DB_DRIVER@@"\
-				  url="jdbc:@@DB_CONNECTOR@@:\/\/localhost:@@DB_PORT@@\/@@TAPAC_DB@@@@DB_SUFFIX@@"\
-				  username="@@TAPAC_USER@@"\
-				  password="@@TAPAC_PASSWORD@@"\
-				  maxActive="20"\
-				  minIdle="5"\/> ' $CATALINA_HOME/conf/server.xml          
+sudo sed -i "/<\/GlobalNamingResources>/i \
+		<Resource name=\"jdbc\/ttv\"\
+				  auth=\"Container\"\
+				  type=\"javax.sql.DataSource\"\
+				  factory=\"org.apache.tomcat.jdbc.pool.DataSourceFactory\"\
+				  uniqueResourceName=\"ttv\"\
+				  driverClassName=\"$DB_DRIVER_DEFAULT\"\
+				  url=\"jdbc:$DB_CONNECTOR_DEFAULT:\/\/localhost:$DB_PORT_DEFAULT\/$TTV_DB$DB_SUFFIX_DEFAULT\"\
+				  username=\"$TTV_USER\"\
+				  password=\"$TTV_PASSWORD\"\
+				  maxActive=\"20\"\
+				  minIdle=\"5\"\/> " $CATALINA_HOME/conf/server.xml      
+
+sudo sed -i "/<\/GlobalNamingResources>/i \
+		<Resource name=\"jdbc\/tapac\"\
+				  auth=\"Container\"\
+				  type=\"javax.sql.DataSource\"\
+				  factory=\"org.apache.tomcat.jdbc.pool.DataSourceFactory\"\
+				  uniqueResourceName=\"tapac\"\
+				  driverClassName=\"$DB_DRIVER_DEFAULT\"\
+				  url=\"jdbc:$DB_CONNECTOR_DEFAULT:\/\/localhost:$DB_PORT_DEFAULT\/$TAPAC_DB$DB_SUFFIX_DEFAULT\"\
+				  username=\"$TAPAC_USER\"\
+				  password=\"$TAPAC_PASSWORD\"\
+				  maxActive=\"20\"\
+				  minIdle=\"5\"\/> " $CATALINA_HOME/conf/server.xml 
+
+sudo sed -i "/<\/bpm-platform>/i \
+	<process-engine name=\"TTV\">\
+		<job-acquisition>default</job-acquisition>\
+		<datasource>java:jdbc/ttv</datasource>\
+		<properties>\
+			<property name=\"databaseTablePrefix\">TTV.</property>\
+			<property name=\"databaseSchemaUpdate\">true</property>\
+			<property name=\"authorizationEnabled\">true</property>\
+			<property name=\"jobExecutorDeploymentAware\">true</property>\
+		</properties>\
+		<plugins>\
+			<plugin>\
+			<class>org.camunda.bpm.application.impl.event.ProcessApplicationEventListenerPlugin</class>\
+			</plugin>\
+		</plugins>\
+	</process-engine> "	$CATALINA_HOME/conf/bpm-platform.xml 	
+
+
+sudo sed -i "/<\/bpm-platform>/i \
+	<process-engine name=\"TAPAC\">\
+		<job-acquisition>default</job-acquisition>\
+		<datasource>java:jdbc/tapac</datasource>\
+		<properties>\
+			<property name=\"databaseTablePrefix\">TAPAC.</property>\
+			<property name=\"databaseSchemaUpdate\">true</property>\
+			<property name=\"authorizationEnabled\">true</property>\
+			<property name=\"jobExecutorDeploymentAware\">true</property>\
+		</properties>\
+		<plugins>\
+			<plugin>\
+			<class>org.camunda.bpm.application.impl.event.ProcessApplicationEventListenerPlugin</class>\
+			</plugin>\
+		</plugins>\
+	</process-engine> "	$CATALINA_HOME/conf/bpm-platform.xml 			  				  
---
+++ multi-tenant/3.install-eform.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# -------
+# This is script to install eform multitenant
+# -------	
+
+# Configure constants
+if [ -f "constants.sh" ]; then
+	. constants.sh
+fi
+
+# Configure colors
+if [ -f "colors.sh" ]; then
+	. colors.sh
+fi
+
+if [ -d "$TMP_INSTALL/workplacebpm-multitenant" ]; then
+	sudo rm -rf $TMP_INSTALL/workplacebpm-multitenant
+fi
+
+git clone https://bitbucket.org/workplace101/workplacebpm.git $TMP_INSTALL/workplacebpm-multitenant
+
+cd $TMP_INSTALL/workplacebpm-multitenant
+git checkout schema-isolation
+
+cd $TMP_INSTALL/workplacebpm-multitenant/src/workflow-plugin-sso
+mvn clean install
+sudo rsync -avz $TMP_INSTALL/workplacebpm-multitenant/src/workflow-plugin-sso/target/workflow-plugin-sso-7.6.1-SNAPSHOT.jar  $CATALINA_HOME/webapps/camunda/WEB-INF/
+	
+sudo rsync -avz $TMP_INSTALL/workplacebpm-multitenant/src/environment/web.xml  $CATALINA_HOME/webapps/camunda/WEB-INF/
+sudo rsync -avz $TMP_INSTALL/workplacebpm-multitenant/src/environment/login.xml  $CATALINA_HOME/webapps/camunda/app/welcome
+
+cd $TMP_INSTALL/workplacebpm-multitenant/src/eForm
+mvn clean install
+sudo rsync -avz $TMP_INSTALL/workplacebpm-multitenant/src/eForm/gateway/target/eform.war  $CATALINA_HOME/webapps/multi-tenant.war
+sleep 10
+
+sudo $DEVOPS_HOME/devops-service.sh start
+
+// Set default user
+mysql -u root -p < $BASE_INSTALL/multi-tenant/create-user.sql
+
+	
