078e5108e56add154235c44a4e1101ec5b3ed1ed
AD\tran.hsp <tran.hsp@tctav.com>
2018-05-31 14:12:30 +0700

    change pgadmin installation dest folder

--- scripts/postgresql.sh
+++ scripts/postgresql.sh
@@ -22,6 +22,7 @@ export ALFRESCO_DB=alfresco
 export ALFRESCO_USER=alfresco
 export CAMUNDA_DB=camunda
 export CAMUNDA_USER=camunda
+export PGADMIN_INSTALLATION_DEST=/home/ubuntu
 
 echo
 echo "--------------------------------------------"
@@ -92,16 +93,16 @@ fi
 read -e -p "Install PostgreSQL Admin (Web)? [y/n] " -i "y" createpgadmin
 if [ "$createpgadmin" = "y" ]; then
 	 sudo apt-get install virtualenv python-pip libpq-dev python-dev
-	 cd $DEVOPS_HOME
+	 cd $PGADMIN_INSTALLATION_DEST
 	 virtualenv pgadmin4
-	 cd $DEVOPS_HOME/pgadmin4
-	 source $DEVOPS_HOME/pgadmin4/bin/activate
-	 curl -# -o $DEVOPS_HOME/pgadmin4/pgadmin4-1.6-py2.py3-none-any.whl	https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v1.6/pip/pgadmin4-1.6-py2.py3-none-any.whl
-	 pip install $DEVOPS_HOME/pgadmin4/pgadmin4-1.6-py2.py3-none-any.whl
-	 python $DEVOPS_HOME/pgadmin4/lib/python2.7/site-packages/pgadmin4/setup.py
+	 cd $PGADMIN_INSTALLATION_DEST/pgadmin4
+	 source $PGADMIN_INSTALLATION_DEST/pgadmin4/bin/activate
+	 curl -# -o $PGADMIN_INSTALLATION_DEST/pgadmin4/pgadmin4-1.6-py2.py3-none-any.whl	https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v1.6/pip/pgadmin4-1.6-py2.py3-none-any.whl
+	 pip install $PGADMIN_INSTALLATION_DEST/pgadmin4/pgadmin4-1.6-py2.py3-none-any.whl
+	 python $PGADMIN_INSTALLATION_DEST/pgadmin4/lib/python2.7/site-packages/pgadmin4/setup.py
 	 deactivate
 	 source bin/activate
-	 PGADMIN4_HOME=$DEVOPS_HOME/pgadmin4
+	 PGADMIN4_HOME=$PGADMIN_INSTALLATION_DEST/pgadmin4
 	 PGADMIN4_HOME_ESC="${PGADMIN4_HOME//\//\\/}"
 	 sudo rsync -avz $BASE_INSTALL/scripts/pgadmin4.service /etc/systemd/system/
 	 sudo sed -i "s/@@PGADMIN4_HOME@@/$PGADMIN4_HOME_ESC/g" /etc/systemd/system/pgadmin4.service
@@ -110,7 +111,8 @@ if [ "$createpgadmin" = "y" ]; then
 	 sudo sed -i "1 i\\#\!\/usr\/bin\/env python" $PGADMIN4_HOME/lib/python2.7/site-packages/pgadmin4/pgAdmin4.py
 	 sudo chmod a+x	$PGADMIN4_HOME/lib/python2.7/site-packages/pgadmin4/pgAdmin4.py
 	 sudo systemctl start pgadmin4
-	 
+	 sudo ufw allow 5050
+	 echogreen "PGAdmin has been installed successfully, you can access via URL : [http/https]://domain_server:5050. "
 	 
 fi
 
